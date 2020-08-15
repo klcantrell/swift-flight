e0438aeb0e7651cbc0d1176b29c4c50befba5b42
klcantrell <cantrellkalalau@gmail.com>
2020-08-14 22:31:40 -0400

    yu/dice: start dice app

---
+++ yu/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    @IBOutlet weak var rollButtonView: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        rollButtonView.layer.cornerRadius = 5
+    }
+
+}
