92833707bc973d2cf2330e9f6645588f18f30fd4
klcantrell <cantrellkalalau@gmail.com>
2020-08-15 21:42:05 -0400

    yu/dice: basic tracking of dice state and action handlers

--- yu/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ yu/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -14,9 +14,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var rollButtonView: UIButton!
     
+    var leftDiceNumber = 1
+    var rightDiceNumber = 5
+    let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         rollButtonView.layer.cornerRadius = 5
     }
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = diceImages[min(leftDiceNumber, 5)]
+        diceImageView2.image = diceImages[max(rightDiceNumber, 0)]
+        leftDiceNumber += 1
+        rightDiceNumber -= 1
+    }
+    
 }
