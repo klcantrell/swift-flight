a4936ea98b67fd26015b096939164221d946cb26
klcantrell <cantrellkalalau@gmail.com>
2020-08-16 20:34:13 -0400

    yu/dice: refactor to use .randomElement() and helper

--- yu/Dicee-iOS13/Dicee-iOS13/ViewController.swift
+++ yu/Dicee-iOS13/Dicee-iOS13/ViewController.swift
@@ -1,11 +1,3 @@
-//
-//  ViewController.swift
-//  Dicee-iOS13
-//
-//  Created by Angela Yu on 11/06/2019.
-//  Copyright © 2019 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
@@ -14,20 +6,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView2: UIImageView!
     @IBOutlet weak var rollButtonView: UIButton!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
     let diceImages = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         rollButtonView.layer.cornerRadius = 5
+        randomizeDiceImageSource(diceImageView1)
+        randomizeDiceImageSource(diceImageView2)
     }
     
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = diceImages[min(leftDiceNumber, 5)]
-        diceImageView2.image = diceImages[max(rightDiceNumber, 0)]
-        leftDiceNumber += 1
-        rightDiceNumber -= 1
+        randomizeDiceImageSource(diceImageView1)
+        randomizeDiceImageSource(diceImageView2)
+    }
+    
+    func randomizeDiceImageSource(_ diceImage: UIImageView) {
+        diceImage.image = diceImages.randomElement()
     }
     
 }
