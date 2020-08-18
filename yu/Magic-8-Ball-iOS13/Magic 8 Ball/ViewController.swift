9b5d5c1637a0cb9af99f48f8f7484706623130ff
klcantrell <cantrellkalalau@gmail.com>
2020-08-17 21:36:52 -0400

    yu/eight-ball: complete eight ball app

--- yu/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
+++ yu/Magic-8-Ball-iOS13/Magic 8 Ball/ViewController.swift
@@ -5,7 +5,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var ballView: UIImageView!
     @IBOutlet weak var askButtonView: UIButton!
     
-    let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
+    let ballImages = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -13,6 +13,6 @@ class ViewController: UIViewController {
     }
     
     @IBAction func askButtonPressed(_ sender: UIButton) {
-        print("Button pressed!")
+        ballView.image = ballImages.randomElement();
     }
 }
