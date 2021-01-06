e0beeaf3a9090f7f9b4fb4e5199a744d8d8f3390
klcantrell <cantrellkalalau@gmail.com>
2021-01-06 14:49:22 -0500

    yu/EggTimer: finish egg timer

--- yu/EggTimer-iOS13/EggTimer/ViewController.swift
+++ yu/EggTimer-iOS13/EggTimer/ViewController.swift
@@ -3,25 +3,29 @@ import UIKit
 class ViewController: UIViewController {
     var countDownId = 0
     
-    let softTime = 3//300
-    let mediumTime = 4//420
-    let hardTime = 7//720
+    let softTime = 300
+    let mediumTime = 420
+    let hardTime = 720
     
     lazy var timeByHardness = ["Soft": softTime, "Medium": mediumTime, "Hard": hardTime]
     
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
     
     @IBAction func hardnessSelected(_ sender: UIButton) {
         guard let hardness = sender.title(for: .normal) else { return }
         guard let time = timeByHardness[hardness] else { return }
         
         countDownId = countDownId + 1
-        titleLabel.text = "Time is a-ticking"
-        countDown(from: time, initialRun: true, id: countDownId)
+        titleLabel.text = "Time is a-ticking for a \(hardness) egg"
+        countDown(from: time, id: countDownId)
     }
     
-    func countDown(from remainingTime: Int, initialRun: Bool, id: Int) {
+    func countDown(from remainingTime: Int, totalTime: Int? = nil, initialRun: Bool = true, id: Int) {
         guard id == countDownId else { return }
+        
+        let actualTotalTime = totalTime ?? remainingTime
+        progressBar.progress = 1.0 - Float(remainingTime) / Float(actualTotalTime)
         
         guard remainingTime > 0 else {
             titleLabel.text = "ALL DONE!"
@@ -32,7 +36,7 @@ class ViewController: UIViewController {
         print("\(remainingTime) \(label)")
         
         DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
-            self.countDown(from: remainingTime - 1, initialRun: false, id: id)
+            self.countDown(from: remainingTime - 1, totalTime: actualTotalTime, initialRun: false, id: id)
         }
     }
 	
