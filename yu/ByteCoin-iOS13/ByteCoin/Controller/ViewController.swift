036652cdfb44d850417b8d7cdc0558fa551a008a
Kal Cantrell <klcantrell@sep.com>
2021-08-11 16:33:01 -0400

    yu/ByteCoin: complete ByteCoin

--- yu/ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
+++ yu/ByteCoin-iOS13/ByteCoin/Controller/ViewController.swift
@@ -1,18 +1,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var bitcoinLabel: UIImageView!
+    @IBOutlet weak var bitcoinLabel: UILabel!
     @IBOutlet weak var currencyLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
 
-    let coinManager = CoinManager()
+    var coinManager = CoinManager()
+    
+    var selectedCurrency = "USD"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         currencyPicker.dataSource = self
         currencyPicker.delegate = self
+        coinManager.delegate = self
     }
+}
+
+extension ViewController: CoinManagerDelegate {
+    func didReceiveData(_ coinManager: CoinManager, coinData: CoinModel) {
+        DispatchQueue.main.async {
+            self.bitcoinLabel.text = coinData.rateString
+            self.currencyLabel.text = self.selectedCurrency
+        }
+    }
+    
+    func didFailWithError(error: Error) {
+        print("OH NO! ERROR: \(error)")
+    }
+    
+    
 }
 
 extension ViewController: UIPickerViewDataSource {
@@ -32,6 +50,7 @@ extension ViewController: UIPickerViewDelegate {
     
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         let selectedCurrency = coinManager.currencyArray[row]
+        self.selectedCurrency = selectedCurrency
         coinManager.getCoinPrice(for: selectedCurrency)
     }
 }
--- yu/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ yu/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -1,9 +1,16 @@
 import Foundation
 
+protocol CoinManagerDelegate {
+    func didReceiveData(_ coinManager: CoinManager, coinData: CoinModel)
+    func didFailWithError(error: Error)
+}
+
 struct CoinManager {
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
     let apiKey = "REDACTED"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    
+    var delegate: CoinManagerDelegate?
     
     func getCoinPrice(for currency: String) {
         let urlString = "\(baseURL)/\(currency)"
@@ -17,10 +24,11 @@ struct CoinManager {
             let session = URLSession(configuration: .default)
             let task = session.dataTask(with: request) { (data, response, error) in
                 if error != nil {
+                    self.delegate?.didFailWithError(error: error!)
                     return
                 }
                 if let safeData = data, let coinData = parseJSON(safeData) {
-                    print(coinData.rateString)
+                    self.delegate?.didReceiveData(self, coinData: coinData)
                 }
             }
             task.resume()
@@ -33,7 +41,7 @@ struct CoinManager {
             let decodedData = try decoder.decode(CoinData.self, from: coinData)
             return CoinModel(rate: decodedData.rate)
         } catch {
-            print("Error decoding coin data: \(error)")
+            self.delegate?.didFailWithError(error: error)
             return nil
         }
     }
