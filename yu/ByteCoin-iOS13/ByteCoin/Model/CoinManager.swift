d5dbffa5623879d061b20b50502351a90796c9d1
Kalalau Cantrell <cantrellkalalau@gmail.com>
2021-08-13 22:30:29 -0400

    Update CoinManager.swift

--- yu/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
+++ yu/ByteCoin-iOS13/ByteCoin/Model/CoinManager.swift
@@ -7,7 +7,7 @@ protocol CoinManagerDelegate {
 
 struct CoinManager {
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "ADF267A5-2F21-41FE-AE0B-AF7BBAD12EE6"
+    let apiKey = "REDACTED"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     
     var delegate: CoinManagerDelegate?
